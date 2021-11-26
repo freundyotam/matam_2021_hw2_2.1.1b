bee3defd1727ff543d32a23717ff3426bce80224
yotam freund <freund.yotam@gmail.com>
2021-11-26 19:25:36 +0200

    Answer for the second question

--- stringduplicator.c
+++ stringduplicator.c
@@ -1,23 +1,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-#include <stdio.h>
 
-char *stringDuplicator(char *s, int times) { // convention: Should be camelcase // convention:DuplicateString
-    assert(s); // Assert should check if the array is null and not if it's not null
+char *duplicateString(char *s, int times)
+{
+    assert(s);
     assert(times > 0);
-    int len = strlen(s); // first bug removing the * // convention: should be lowercase
+    int len = strlen(s);
     char *out = malloc(len * times);
-    char *beginning = out; // We want to return beginning and not out since its modified
-    assert(out); // This shouldn't be an 'assert' but 'if(out == NULL)'
+    if(out == NULL){
+        return NULL;
+    }
+    char *beginning = out;
+    assert(out);
     for (int i = 0; i < times; i++) {
-        strcpy(out, s); // First put s then increment out
+        strcpy(out, s);
         out = out + len;
     }
     return beginning;
-}
-
-int main(){
-    char str[] = "1234";
-    printf("%s", stringduplicator(str,1));
 }
