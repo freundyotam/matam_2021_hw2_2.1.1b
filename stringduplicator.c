17b7a42ed25b680a69287a1dc91fc3b52e0d7e98
yotam freund <freund.yotam@gmail.com>
2021-11-26 18:10:57 +0200

    Add convention comments

--- stringduplicator.c
+++ stringduplicator.c
@@ -3,16 +3,16 @@
 #include <assert.h>
 #include <stdio.h>
 
-char *stringduplicator(char *s, int times) {
+char *stringDuplicator(char *s, int times) { // convention: Should be camelcase // convention:DuplicateString
     assert(s); // Assert should check if the array is null and not if it's not null
     assert(times > 0);
-    int LEN = strlen(s); // first bug removing the *
-    char *out = malloc(LEN * times);
+    int len = strlen(s); // first bug removing the * // convention: should be lowercase
+    char *out = malloc(len * times);
     char *beginning = out; // We want to return beginning and not out since its modified
     assert(out); // This shouldn't be an 'assert' but 'if(out == NULL)'
     for (int i = 0; i < times; i++) {
         strcpy(out, s); // First put s then increment out
-        out = out + LEN;
+        out = out + len;
     }
     return beginning;
 }
